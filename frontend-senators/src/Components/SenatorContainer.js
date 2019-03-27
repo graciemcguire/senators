87b1b9e24bc3408877f6aade3fc112167b950657
github username <github email address>
2019-03-27 17:40:35 -0400

    started search

--- frontend-senators/src/Components/SenatorContainer.js
+++ frontend-senators/src/Components/SenatorContainer.js
@@ -1,34 +1,65 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import SenatorThumbnail from './SenatorThumbnail'
 import Nav from './Nav'
+import SearchForm from './SearchForm'
 
-const SenatorContainer = props => {
+class SenatorContainer extends Component  {
 
-  return(
-    <Fragment>
-      <Nav />
-    <div className='title'>
-      <center><h1>2019 US Senators</h1></center>
-    </div>
-    <div className= 'senator-grid'>
+  state = {
+    searchTerm: '',
+    clicked: false
+  }
 
-      { props.senators.senators ?
-        props.senators.senators.map(senator => (
+  filterSenators = () => {
+    return this.props.senators.senators ?
+      this.props.senators.senators.filter(senator =>
+      senator.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())) : console.log('no')
+  }
+
+  changeHandler = event => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+    this.filterSenators()
+  }
+
+
+  render(){
+    console.log(this.state)
+    const { senators } = this.props
+
+    const renderSenators = () => {
+      return senators.senators ?
+        senators.senators.map(senator => (
         <SenatorThumbnail
         key={ senator.id }
         senator={ senator }/>
-      )) : console.log(props.senators) }
-    </div>
-    </Fragment>
-  )
-}
+      )) : console.log(senators)
+    }
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.auth.user,
-    senators: state.senators
+    return(
+      <Fragment>
+        <Nav />
+      <div className='title'>
+        <center><h1>2019 US Senators</h1></center>
+        <center><SearchForm className='btn' changeHandler={ this.changeHandler }/></center>
+      </div>
+
+      <div className= 'senator-grid'>
+        { renderSenators() }
+      </div>
+      </Fragment>
+    )
   }
 }
+
+  const mapStateToProps = (state) => {
+    return {
+      user: state.auth.user,
+      senators: state.senators
+    }
+  }
+
 
 export default connect(mapStateToProps)(SenatorContainer);
