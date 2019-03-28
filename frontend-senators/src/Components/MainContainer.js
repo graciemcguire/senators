f3f04ef5be2e01d8c3c1374dd29b965cce612e11
github username <github email address>
2019-03-27 23:32:22 -0400

    fixed swipe

--- frontend-senators/src/Components/MainContainer.js
+++ frontend-senators/src/Components/MainContainer.js
@@ -1,47 +1,95 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom'
 import { connect } from 'react-redux'
-import TestSwipe from './TestSwipe'
+import { handleCreateRating } from '../Redux/userActions'
 import Nav from './Nav'
+import { Card, CardWrapper } from 'react-swipeable-cards';
 
-const MainContainer = props => {
+class MainContainer extends Component {
 
-const renderPage = () => {
-  if(props.senators){
-    return(
-      <Fragment>
-      <div className='main'>
-        <Nav />
-        <div className='senator-cards'>
-          { props.senators.senators ?
-            props.senators.senators.map(senator => (
-            <TestSwipe
-            key={ senator.id }
-            senator={ senator }/>
-          )) : console.log(props.senators) }
-        </div>
-      </div>
-      </Fragment>
-    )
-  } else {
-    return <center><h1>L O A D I N G . . .</h1></center>
+  onSwipeLeft = (sen) => {
+    console.log("I was swiped left.");
   }
-}
 
-    console.log('main', props);
+  onSwipeRight = (sen) => {
+    console.log("I was swiped right.", this.props);
+  }
+
+  renderPage = () => {
+    console.log(this.props.senators.senators);
+
+    let titleStyle = {
+			textAlign: "center",
+			fontSize: "30px",
+			fontFamily: "Serif",
+			marginTop: "10px",
+      color: "#00293C",
+      display: 'inline-block'
+		};
+
+    if(this.props.senators.senators){
+      return(
+        <Fragment>
+          <Nav />
+          <div>
+            <div style={ this.containerStyle }>
+              <CardWrapper style={ this.wrapperStyle }>
+              { this.props.senators.senators ?
+                this.props.senators.senators.map(senator => (
+                <Card
+                key={ senator.id }
+                senator={ senator }
+                onSwipeLeft={ this.onSwipeLeft.bind(this) }
+                onSwipeRight={ this.onSwipeRight.bind(this)}
+                >
+              <div style={ titleStyle }>
+                <img className='card-image' src={ senator.image } alt={ senator.name }/>
+                <h2>{ senator.name }</h2>
+                <h3>{ senator.party } - { senator.state }</h3>
+              </div>
+                </Card>
+              )) : console.log(this.props.senators) }
+              </CardWrapper>
+            </div>
+          </div>
+        </Fragment>
+      )
+    } else {
+      return <center><h1>L O A D I N G . . .</h1></center>
+    }
+  }
+  render(){
+
+    let wrapperStyle = {
+      backgroundColor: "#024773",
+      height: "80vh"
+    };
+
+    let containerStyle = {
+      backgroundColor: "green",
+      height: "calc(100vh - 16px)"
+    }
+
     return(
       <div>
-      { renderPage() }
+      { this.renderPage() }
       </div>
     )
+  }
 }
-
 
 const mapStateToProps = (state) => {
   return {
     user: state.auth.user,
-    senators: state.senators
+    senators: state.senators,
+    user_senators: state.user_senators
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleCreateRating: (userId, senatorId, wokeOrJoke) => dispatch(handleCreateRating(userId, senatorId, wokeOrJoke))
+  }
+}
 
-export default connect(mapStateToProps)(MainContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
--- frontend-senators/src/Components/TestSwipe.js
+++ frontend-senators/src/Components/TestSwipe.js
@@ -6,45 +6,81 @@ import { connect } from 'react-redux';
 
 class TestSwipe extends Component {
 
-  onSwipe(data) {
+  onSwipe(sen) {
     console.log("I was swiped.");
   }
 
-  onSwipeLeft(data) {
+  onSwipeLeft(sen) {
     console.log("I was swiped left.");
   }
 
-  onSwipeRight(data) {
+  onSwipeRight(sen) {
     console.log("I was swiped right.");
   }
+
+  renderPartyColor = () => {
+    return this.props.senator.party === 'R' ?
+    'senator-card-gop' : 'senator-card-dem'
+  }
+
+  cardStyle = {
+    backgroundColor: "#F1F3CE",
+    textAlign: 'center'
+   }
+
 
   renderCards() {
     const { senator } = this.props
     console.log(this.props);
     let sen = [this.props.senator]
+    let titleStyle = {
+			textAlign: "center",
+			fontSize: "30px",
+			fontFamily: "Serif",
+			marginTop: "10px",
+      color: "#00293C",
+      display: 'inline-block'
+		};
      return sen.map((sen) => {
       return(
         <Card
+          style={ this.cardStyle }
           key={ sen.id }
           onSwipe={ this.onSwipe.bind(this) }
           onSwipeLeft={ this.onSwipeLeft.bind(this) }
           onSwipeRight={ this.onSwipeRight.bind(this)} >
+
+          <div style={ titleStyle }>
+
             <img className='card-image' src={ senator.image } alt={ senator.name }/>
             <h2>{ senator.name }</h2>
             <h3>{ senator.party } - { senator.state }</h3>
+          </div>
         </Card>
       );
     });
   }
 
+
   render() {
+
+    let wrapperStyle = {
+      backgroundColor: "#024773",
+      height: "80vh"
+    };
+    let containerStyle = {
+      backgroundColor: "green",
+      height: "calc(100vh - 16px)"
+    }
+
     return(
-      <CardWrapper>
-        {this.renderCards()}
-      </CardWrapper>
+      <div>
+          { this.renderCards() }
+      </div>
     );
   }
 }
+
 
 const mapStateToProps = (state) => {
   return {
